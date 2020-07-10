90e5870ccf14b24413b5ff74329ed224b70add08
James Trinklein <1398935+jtrinklein@users.noreply.github.com>
2020-07-10 12:06:12 -0700

    update gi to track count of use

--- helpers/gi.sh
+++ helpers/gi.sh
@@ -1,9 +1,27 @@
 #!/usr/bin/env bash
-export GI_VERSION="1.0.1"
-GI_LOGO="$HOME/.dotfiles/helpers/gilogo.txt"
+export GI_VERSION="1.1.0"
+GI_CF="$HOME/.gi"
 
 function gi() {
-
+    local count=0
+    [ -f "$GI_CF" ] && count=$( cat "$GI_CF" )
+    count=$( expr ${count:-0} + 1 )
+    echo -n "$count" > "$GI_CF"
+    local post='th'
+    case "${count:(-1)}" in
+        1)
+            post='st'
+            [[ ${count} == *'11' ]] && post='th'
+            ;;
+        2)
+            post='nd'
+            [[ ${count} == *'12' ]] && post='th'
+            ;;
+        3)
+            post='rd'
+            [[ ${count} == *'13' ]] && post='th'
+            ;;
+    esac
     cat <<EOF
        ___________   __________         __     _______    ______
       /  ________/ /__     ___/       /  /   /   _   /  /    __/
@@ -17,10 +35,11 @@ function gi() {
 ┌─┬────────────────────────────────────────────────────────┬─┐
 │ │                      Git Typo HERO                     │ │
 └─┴────────────────────────────────────────────────────────┴─┘
+
+         Coming to your rescue for the ${count}${post} time.
                         Version: $GI_VERSION
 
 EOF
-
     echo "Converting your: gi $@"
     local action="$( echo $1 | awk '/^t/ { print substr($1, 2); }' )"
     shift
